f3afe53ac58a04ad9c8f8efe67010a06c93c10db
Dolev <dolev.raziel@gmail.com>
2025-06-10 12:31:49 +0300

    basic task showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Task } from './components/Task'
+import { MantineProvider } from '@mantine/core'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <MantineProvider>
+      <Task description={"dummy desc"} id={1} status={"in progress"} title={"basic task"} key={1}></Task>
+
+    </MantineProvider>
     </>
   )
 }
