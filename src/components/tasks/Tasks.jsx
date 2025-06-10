c762308e264a39b9aee9b6ae72d1c8a0a51828f9
Dolev <dolev.raziel@gmail.com>
2025-06-10 14:22:17 +0300

    implemented basic fetch from api

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -1,10 +1,26 @@
 import { Grid } from "@mantine/core";
 import { Task } from "../task/Task";
+import {useState, useEffect} from "react";
+
+import axios from "axios";
 
 export const Tasks = () => {
+
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const tasksFetched = await axios.get("http://localhost:3001/api/tasks");
+            setTasks(tasksFetched.data);
+        })()
+    }, [])
+
     return <Grid justify="space-around">
-        <Grid.Col style={{maxWidth: 350}} sm={4} xs={4}>
-            <Task/>
-        </Grid.Col>
+        {tasks.map((task, key) => 
+            <Grid.Col style={{maxWidth: 350}} sm={4} xs={4}>
+                <Task task/>
+            </Grid.Col>
+        )}
+        
     </Grid>
 }
